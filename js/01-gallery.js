7c16915196200d95d0005ebd61debefa45f3080f
OleksiiVTS <119164270+OleksiiVTS@users.noreply.github.com>
2023-04-20 17:18:43 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line 
 
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+const createMakup = createFotoGalery(galleryItems);
+
+ulGallery.insertAdjacentHTML('beforeend', createMakup);
+ulGallery.addEventListener("click", openBigImg);
+
+function createFotoGalery(galleryItems) {
+    return galleryItems
+        .map(({description, original, preview}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+            <img
+            class="gallery__image"
+            src="${original}"
+            data-source="${preview}"
+            alt="${description}"
+            />
+        </a>
+        </li>
+        `;
+        })
+        .join(""); 
+};
+
+function openBigImg(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== "IMG") {
+        return;
+    };
+    const urlBigImg = event.target.dataset.source
+
+    openModalWindows(urlBigImg);
+};
+
+function openModalWindows(url) {
+    document.addEventListener("keydown", lisenerEscape)
+    console.dir(url)
+};
+
+function closeModalWindows(params) {
+    document.removeEventListener("keydown", lisenerEscape)
+    
+};
+
+function lisenerEscape(event) {
+    if (event.code !== "Escape") {
+      return;  
+    };
+    console.log("Escape")
+    // if (condition) {
+    //     closeModalWindows();
+    // }
+    
+};
+console.log(ulGallery);
